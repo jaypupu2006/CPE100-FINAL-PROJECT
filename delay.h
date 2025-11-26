578397948abd00ce3c3eaef6ba9148d65ff60855
Jaypupu2006 <dffda344567@gmail.com>
2025-11-26 17:46:24 +0700

    Clear all files in finalisus branch

--- delay.h
+++
@@ -1,8 +0,0 @@
-#ifndef DELAY_H
-#define DELAY_H
-
-#include <time.h>
-
-void delay(int number_of_seconds);
-
-#endif
--- menu1.h
+++
@@ -1,5 +0,0 @@
-void input_file();
-void register_member();
-void search_member();
-void show_all_members();
-void menu1_choose();
